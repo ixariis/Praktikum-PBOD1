4b2854fe4e628eefa4bd3e955c529c4e39dbf12a
ixariis <119769875+ixariis@users.noreply.github.com>
2024-03-13 17:25:23 +0700

    Add files via upload

---
+++ Praktikum 4/direktori_anda/org/poligon/Poligon.java
@@ -0,0 +1,15 @@
+/* Nama File    : Poligon.java
+ * NIM | Nama   : 24060122140107 | Surya Fajar
+ * Deskripsi    : representasi dasar dari objek poligon (Segi Banyak)
+ */
+
+ package org.poligon;
+
+ public class Poligon{
+    protected int jumlahSisi;
+
+    public int getjumlahSisi(){
+        return this.jumlahSisi;
+    }
+ }
+ 
